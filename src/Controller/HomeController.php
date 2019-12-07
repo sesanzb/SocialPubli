d2db793ca808e0c7aa31cc2d350c6dbe368d293b
Sergio Sanz Barrio <sesanzb@gmail.com>
2019-12-07 01:51:53 +0100

    Cleaning HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends AbstractController{
      * @Route("/", name="homepage") 
      */ 
     public function index (Request $request, SwapiService $swapi) {
-        $template = ($request->isXmlHttpRequest() || $request->query->get('showJson') == 1) ? 'home/_table.html.twig' : 'home/index.html.twig';
+        $template = $request->isXmlHttpRequest() ? 'home/_table.html.twig' : 'home/index.html.twig';
         $query_string = $request->getQueryString() ?? '';
         return $this->render($template, [
                'response' => json_decode($swapi->getPeople($query_string), true),
